6bc0269f34c4c20b8bc4c92b739797a582d9693a
redhotrocknrolla <jimbo_jo@mail.ru>
2021-11-22 20:14:48 +0300

    hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import javax.transaction.*;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    Transaction transaction = null;
+    SessionFactory sessionFactory = Util.SesFac();
+    Session session = sessionFactory.openSession();
     public UserDaoHibernateImpl() {
 
     }
@@ -12,7 +19,22 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        transaction = (Transaction) session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS users(id BIGINT NOT NULL AUTO_INCREMENT," +
+                            "name VARCHAR (45)NOT NULL, lastname VARCHAR (45) NOT NULL," +
+                        "age TINYINT NOT NULL, PRIMARY KEY (id))").
+                        executeUpdate();
+        try {
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null){
+                try {
+                    transaction.rollback();
+                } catch (SystemException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,20 +3,49 @@ package jm.task.core.jdbc.util;
 
 
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
     private static final String Url = "jdbc:mysql://localhost/new_schema";
     private static final String Username = "root";
     private static final String Password = "76315as";
-    
+    private static final String Driver = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory SesFac(){
+        try {
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, Driver);
+            settings.put(Environment.URL, Url);
+            settings.put(Environment.USER, Username);
+            settings.put(Environment.PASS, Password);
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+            sessionFactory = new Configuration().setProperties(settings).addAnnotatedClass(User.class).buildSessionFactory();
+        } catch (Exception exception) {
+            throw new ExceptionInInitializerError();
+
+        } return sessionFactory;
+    }
+
+
+
+
+
+
 
     public static Connection getConnection()  {
         Connection connection = null;
-
         try {
             connection = DriverManager.getConnection(Url, Username, Password);
             connection.setAutoCommit(false);
